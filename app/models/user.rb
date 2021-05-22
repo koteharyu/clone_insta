f0aaebee16bc3eff48ddde34175852fdb403741c
haryu <hryau6@gmail.com>
2021-05-22 15:45:46 +0900

    add: own? method to models/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   has_many :posts, dependent: :destroy
+
+  def own?(post)
+    id == post.user_id
+  end
 end
