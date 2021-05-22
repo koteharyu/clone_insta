615cc14be35bbcda1bdaf18f98a820812e834379
haryu <hryau6@gmail.com>
2021-05-22 13:05:37 +0900

    add: user_sessions function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require popper
 //= require rails-ujs
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
 //= require activestorage
