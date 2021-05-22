e86a3c259e56c31334c681ec65b1ca4548395125
haryu <hryau6@gmail.com>
2021-05-22 13:35:16 +0900

    add: header with logged in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'user_sessions#new'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
 
   resources :users
 end
