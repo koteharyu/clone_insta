fd6d055485ba7c48a5e1281555a91c40a79a4e56
haryu <hryau6@gmail.com>
2021-05-22 16:06:12 +0900

    fix: rootの設定をposts#indexに統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  constraints -> request { request.session[:user_id].present? } do
-    root 'posts#index'
-  end
-  root 'user_sessions#new'
+  root 'posts#index'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
