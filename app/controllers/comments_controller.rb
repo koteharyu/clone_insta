f9b2604d08e57e5754fd4fe9beb8a51eccf6c16d
haryu <hryau6@gmail.com>
2021-05-24 18:01:30 +0900

    add: function of destoy my comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,11 @@ class CommentsController < ApplicationController
     @comment.update(comment_update_params)
   end
 
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body).merge(post_id: params[:post_id])
