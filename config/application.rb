f0b045d34f0180e40ada708778b6616466ea2dd7
haryu <hryau6@gmail.com>
2021-05-24 21:10:07 +0900

    fix: removed swearjar from application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require 'swearjar' # NGワード設定用
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
