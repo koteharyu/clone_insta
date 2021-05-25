a629206296b12f213051082160b3b3f8369503b5
haryu <hryau6@gmail.com>
2021-05-25 18:48:04 +0900

    add: recent scope to mdoels/user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc).page(params[:page])
+    @users = User.recent(5)
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,8 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  scope :recent, -> (count) { order(created_at: :desc)}
+
   def own?(object)
     id == object.user_id
   end
