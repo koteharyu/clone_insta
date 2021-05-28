24ed57a0a0f4831e2122efeb1bfaed1d6e6da308
haryu <hryau6@gmail.com>
2021-05-28 19:43:16 +0900

    add: rails db:migrate

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: notifications
+#
+#  id            :integer          not null, primary key
+#  read          :boolean          default(FALSE), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  noticeable_id :integer
+#  user_id       :integer
+#
+# Indexes
+#
+#  index_notifications_on_noticeable_id  (noticeable_id)
+#  index_notifications_on_user_id        (user_id)
+#
 class Notification < ApplicationRecord
   belongs_to :noticeable
   belongs_to :user
