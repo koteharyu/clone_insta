a3f39180eb0ea8bb63f6df5519808e8e3c8cdc4f
haryu <hryau6@gmail.com>
2021-05-28 18:25:08 +0900

    add: rails g migration AddAvatarToUsers avatar:string

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id               :integer          not null, primary key
+#  avatar           :string
 #  crypted_password :string
 #  email            :string           not null
 #  name             :string           not null
