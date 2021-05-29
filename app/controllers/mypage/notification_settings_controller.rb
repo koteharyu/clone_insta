2d936ad680157c0feabc10848bdf679d80f6dc0e
haryu <hryau6@gmail.com>
2021-05-29 15:47:38 +0900

    clear: rubocop -a

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -8,12 +8,13 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
     if @user.update(notification_settings_params)
       redirect_to edit_mypage_notification_setting_path, success: 'メール通知設定を更新しました'
     else
-      flash.now[:danger] = "メール通知設定の更新に失敗しました"
+      flash.now[:danger] = 'メール通知設定の更新に失敗しました'
       render :edit
     end
   end
 
   private
+
   def notification_settings_params
     params.require(:user).permit(:notification_on_comment, :notification_on_like, :notification_on_follow)
   end
