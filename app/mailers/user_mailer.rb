f9016e132de4dece2787dcb54c46286ac68079cc
haryu <hryau6@gmail.com>
2021-05-29 13:05:18 +0900

    add: configuretaion about letter opener web

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,7 +10,7 @@ class UserMailer < ApplicationMailer
     @user_from = params[:user_from]
     @user_to = params[:user_to]
     @post = params[:post]
-    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました"
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました")
   end
 
   def follow
--- config/routes.rb
+++ config/routes.rb
@@ -60,5 +60,6 @@ Rails.application.routes.draw do
     resources :notifications, only: :index
   end
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
 end
