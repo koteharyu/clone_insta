326e7b3b22467e5adb5870f9681a41304d2dc967
haryu <hryau6@gmail.com>
2021-05-29 14:33:23 +0900

    add: mount sidekiq::web

--- config/routes.rb
+++ config/routes.rb
@@ -60,5 +60,8 @@ Rails.application.routes.draw do
     resources :notifications, only: :index
   end
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+    mount Sidekiq::Web, at: 'sidekiq'
+  end
 end
