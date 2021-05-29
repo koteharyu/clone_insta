951e0e3e4bf8129bcfe198760130831a983c1e81
haryu <hryau6@gmail.com>
2021-05-29 12:45:01 +0900

    add: 3 methods to user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,21 @@
 class UserMailer < ApplicationMailer
+  def comment_post
+    @user_from = params[:user_from]
+    @user_to = params[:user_to]
+    @comment = params[:comment]
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にコメントしました")
+  end
+
+  def like_post
+    @user_from = params[:user_from]
+    @user_to = params[:user_to]
+    @comment = params[:comment]
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました"
+  end
+
+  def follow
+    @user_from = parmas[:user_from]
+    @user_to = params[:user_to]
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたをフォローしました")
+  end
 end
