d3a4350b6f3516cd6774207e836a40478dcdaaa0
haryu <hryau6@gmail.com>
2021-05-29 13:43:59 +0900

    clean: rubicop -a

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,5 @@ Rails.application.routes.draw do
     resources :notifications, only: :index
   end
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
