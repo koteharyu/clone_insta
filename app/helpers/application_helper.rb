a1e6cedc50b3f2fff76bb8be01a4ff0307cb8227
haryu <hryau6@gmail.com>
2021-05-29 16:33:31 +0900

    add: display_meta_tag to application.html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: Settings.meta.site,
+      reverse: true,
+      title: Settings.meta.title,
+      description: Settings.meta.description,
+      keywords: Settings.meta.keywords,
+      canonical: request.original_url,
+      og: {
+        title: :full_title,
+        type: Settings.meta.og.type,
+        url: request.original_url,
+        image: image_url(Settings.meta.og.image_url),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: {
+          _: Settings.meta.og.image_path,
+        }
+      }
+    }
+  end
 end
