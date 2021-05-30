3e89c278e165fd5ca2ad63f99097772717b08e59
haryu <hryau6@gmail.com>
2021-05-30 19:10:20 +0900

    add: annotate

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  images     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                      :integer          not null, primary key
+#  avatar                  :string
+#  crypted_password        :string
+#  email                   :string           not null
+#  name                    :string           not null
+#  notification_on_comment :boolean          default(TRUE)
+#  notification_on_follow  :boolean          default(TRUE)
+#  notification_on_like    :boolean          default(TRUE)
+#  salt                    :string
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
