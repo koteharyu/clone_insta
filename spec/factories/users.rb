7e840ffa8404eae62cdd32f503aaa9c5e3f5c75a
haryu <hryau6@gmail.com>
2021-05-30 19:00:47 +0900

    add: factrories/user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
+    email { Faker::Interner.email }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
