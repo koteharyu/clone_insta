bb9436e4590f4072dffc0d201ed0d6dd51aaa7ba
haryu <hryau6@gmail.com>
2021-06-03 18:24:26 +0900

    add: search method to searchpostsform

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -7,8 +7,15 @@ class SearchPostsForm
   attribute :user_name, :string
 
   def search
-    scope = Post.all
-    scope = scope.body_contain(body) if body.present?
+    scope = Post.distinct
+    scope = split_post_body.map{ |word| scope.post_like(word) }.inject{ |result, scp| result.or(scp) } if post_body.present?
+    scope = scope.comment_like(comment_body) if comment_body.present?
+    scope = scope.user_like(user_name) if user_name.present?
     scope
+  end
+
+  private
+  def split_post_body
+    post_body.stripe.split(/[[:blank:]]+/)
   end
 end
