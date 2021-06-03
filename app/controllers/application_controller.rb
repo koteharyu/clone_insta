a40881fbd673d243ac5fafe80d1c5bdb3292d7cb
haryu <hryau6@gmail.com>
2021-06-03 18:28:04 +0900

    add: search method to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   add_flash_types :waring, :success, :info, :danger
 
   before_action :require_login
+  before_action :set_search_form
+
+  def set_search_form
+    @search_form = SearchPostsForm.new(search_params)
+  end
+
+  def search_params
+    params.fetch(:search, {}).permit(:post_body, :comment_body, :user_name)
+  end
 
   protected
 
