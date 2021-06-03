cd99b0dad57e7d6377d4cde25b639efbfee54580
haryu <hryau6@gmail.com>
2021-06-03 20:32:40 +0900

    add: mdoels/user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,69 +22,127 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validation' do
-    context 'when validation is ok' do
-      it 'valids when name, email, password exist' do
-        expect(build(:user)).to be_valid
-      end
+    it 'is valid with name, email, pass, pass_con' do
+      expect(build(:user)).to be_valid
     end
 
-    context 'when validation is NG' do
-      it 'is not valid without name' do
-        user = build(:user, name: nil)
-        user.valid?
-        expect(user.errors[:name]).to include("を入力してください")
-      end
+    it 'is invalid without name' do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include "を入力してください"
+    end
 
-      it 'is not valid without email' do
-        user = build(:user, email: nil)
-        user.valid?
-        expect(user.errors[:email]).to include("を入力してください")
-      end
+    it 'is invalid without email' do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include "を入力してください"
+    end
 
-      it 'is not valid without password' do
-        user = build(:user, password: nil)
-        user.valid?
-        expect(user.errors[:password]).to include("は3文字以上で入力してください")
-      end
+    it 'is invalid without password' do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include "は3文字以上で入力してください"
+    end
 
-      it 'is not valid without password_confirmation' do
-        user = build(:user, password_confirmation: nil)
-        user.valid?
-        expect(user.errors[:password_confirmation]).to include("を入力してください")
-      end
+    it 'is invalid without password_confirmation' do
+      user = build(:user, password_confirmation: nil)
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include "を入力してください"
+    end
 
-      it 'is not valid when password and password_confirmation are different'do
-        user = build(:user, password: "password", password_confirmation: "abc")
-        user.valid?
-        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
-      end
+    it 'is invalid with same email' do
+      user = create(:user, email: "spec@example.com")
+      other_user = build(:user, email: "spec@example.com")
+      other_user.valid?
+      expect(other_user.errors[:email]).to include "はすでに存在します"
     end
   end
 
-    describe 'scope :recent' do
-      context '2020年4月1日から10日まで、毎日userを１人ずつ作成した場合' do
-        let(:date) { Date.new(2020, 4, 1) }
-        let!(:users) {  10.times {  |n| create(:user, created_at: date + n, updated_at: date + n) } }
+  describe 'instance method ' do
+    let!(:user) {create(:user)}
+    let!(:other_user) {create(:user)}
+    let!(:user_post) {create(:post, user: user)}
+    let!(:other_post) {create(:post, user: other_user)}
 
-        context 'recent(5)' do
-          it 'return 5 users' do
-            expect(User.recent(5).count).to eq 5
-          end
+    describe 'own?' do
+      it 'returns true when user.own? user_post' do
+        expect(user.own?(user_post)).to be true
+      end
 
-          it 'number 5 users created_at is 20200406' do
-            expect(User.recent(5)).to include User.find_by(created_at: '20200406')
-          end
-
-          it 'number 6 user in not including' do
-            expect(User.recent(5)).to_not include User.find_by(created_at: '20200405')
-          end
-        end
-
-        context 'recent(10)' do
-          it 'return 10 users' do
-            expect(User.recent(10).count).to eq 10
-          end
-        end
+      it 'returens false when user.own? other_post' do
+        expect(user.own?(other_post)).to be false
       end
     end
+
+    describe 'like' do
+      it 'likes other post' do
+        expect{user.like(other_post)}.to change{other_post.likes.count}.by(1)
+      end
+
+      it 'unlike other post' do
+        user.like(other_post)
+        expect{user.unlike(other_post)}.to change{other_post.likes.count}.by(-1)
+      end
+
+      it 'is true that user like other post' do
+        user.like(other_post)
+        expect(user.like?(other_post)).to be true
+      end
+
+      it 'is false that user like other post' do
+        expect(user.like?(other_post)).to be false
+      end
+    end
+
+    describe 'follow' do
+      it 'is increasing other_user.followers for user follow other user' do
+        expect{user.follow other_user}.to change{other_user.followers.count}.by(1)
+      end
+
+      it 'is increasing user.following for user follow other user' do
+        expect{user.follow other_user}.to change{user.following.count}.by(1)
+      end
+
+      it 'user unfollow other user' do
+        user.follow(other_user)
+        expect{user.unfollow(other_user)}.to change{ other_user.followers.count}.by(-1)
+      end
+
+      it 'user unfollow other user' do
+        user.follow(other_user)
+        expect{user.unfollow(other_user)}.to change{ user.following.count}.by(-1)
+      end
+
+      it 'is true that user is following other user' do
+        user.follow other_user
+        expect(user.follow?(other_user)).to be true
+      end
+
+      it 'is false that user is following other user' do
+        expect(user.follow?(other_user)).to be false
+      end
+    end
+
+    describe 'feed' do
+      before do
+        user.follow(other_user)
+      end
+
+      it 'user feed have user post' do
+        expect(user.feed).to include user_post
+      end
+
+      it 'user feed have other user post' do
+        expect(user.feed).to include other_post
+      end
+
+      it 'user feed not have other2 post' do
+        user2 = create(:user)
+        other2_post = create(:post, user: user2)
+        expect(user.feed).to_not include other2_post
+      end
+    end
+
+  end
+
 end
