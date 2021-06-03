0385f966e828933ce0e4f2ffebce5a19628135c2
haryu <hryau6@gmail.com>
2021-06-03 18:15:40 +0900

    add: 2 attribtues to searchpostsform

--- app/forms/search_posts_form.rb
+++ app/forms/search_posts_form.rb
@@ -2,7 +2,9 @@ class SearchPostsForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  attribute :body, :string
+  attribute :post_body, :string
+  attribute :comment_body, :string
+  attribute :user_name, :string
 
   def search
     scope = Post.all
