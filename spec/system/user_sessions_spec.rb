ec08acbf9b217815cb74808b2887b7651e8d2d79
haryu <hryau6@gmail.com>
2021-06-07 19:14:57 +0900

    add: system/user_sessions

---
+++ spec/system/user_sessions_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'UserSessions', type: :system do
+  let(:user) { create(:user) }
+  describe 'login' do
+    context 'correct' do
+      it 'login' do
+        visit login_path
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "password"
+        click_button "ログイン"
+        expect(current_path).to eq root_path
+        expect(page).to have_content "ログインしました"
+      end
+    end
+
+    context 'faild' do
+      it 'can not login' do
+        visit login_path
+        fill_in "メールアドレス", with: user.email
+        fill_in "パスワード", with: "ppppppp"
+        click_button "ログイン"
+        expect(current_path).to eq login_path
+        expect(page).to have_content "ログインに失敗しました"
+      end
+    end
+  end
+
+  describe 'login' do
+    it 'redirects to login path' do
+      login(user)
+      click_on "ログアウト"
+      expect(current_path).to eq login_path
+      expect(page).to have_content "ログアウトしました"
+    end
+  end
+end
