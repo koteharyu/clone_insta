20bbf85fcdd44bcb2e5384a260257a86c562c7d2
haryu <hryau6@gmail.com>
2021-06-19 13:07:13 +0900

    add: self.chatroom_for_users in models/chatroom

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -11,4 +11,18 @@ class Chatroom < ApplicationRecord
   has_many :chatroom_users, dependent: :destroy
   has_many :users, through: :chatroom_users
   has_many :messages, dependent: :destroy
+
+  def self.chatroom_for_users(users)
+    user_ids = users.map(&:id).sort
+    name = user_ids.join(':').to_s
+
+    if chatroom = where(name: name).first
+      chatroom
+    else
+      chatroom = new(name: name)
+      chatroom.users = users
+      chatroom.save
+      chatroom
+    end
+  end
 end
