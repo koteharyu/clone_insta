7733896cfc15f3353fd5c5753bc8bb9a7d080af6
haryu <hryau6@gmail.com>
2021-06-19 16:14:31 +0900

    add: cookies to user_sessions_controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,6 +6,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
+      cookies.signed["user.id"] = current_user.id
       redirect_back_or_to root_path, success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
@@ -15,6 +16,7 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
+    cookies.delete("user.id")
     redirect_to login_path, success: 'ログアウトしました'
   end
 end
