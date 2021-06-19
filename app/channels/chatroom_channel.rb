6c865a975503c95b5cc725fa0728f4a8f4db47ac
haryu <hryau6@gmail.com>
2021-06-19 16:09:20 +0900

    add: subscribed of  chatroom_channel

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,6 +1,6 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chatroom_#{params[:chatroom_id]}"
   end
 
   def unsubscribed
