528ed1523b746f993f20f426cfcb119ae758c8d7
haryu <hryau6@gmail.com>
2021-06-19 16:43:02 +0900

    add: messages/edit.js.slim

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,7 +9,7 @@ class ChatroomsController < ApplicationController
     users = User.where(id: params.dig(:chatroom, :user_ids)) + [current_user]
     @chatroom = Chatroom.chatroom_for_users(users)
     # @messages = @chatroom.messages.order(created_at: :desc).limit(100).reverse
-    @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
+    # @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
     redirect_to chatroom_path(@chatroom)
   end
 
