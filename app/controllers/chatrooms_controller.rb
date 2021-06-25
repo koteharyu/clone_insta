d54fade9679cf6c672407007c2b36f3f184e73d5
haryu <hryau6@gmail.com>
2021-06-25 13:48:28 +0900

    add: chatrooms_spec

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -9,9 +9,6 @@ class ChatroomsController < ApplicationController
   def create
     users = User.where(id: params.dig(:chatroom, :user_ids)) + [current_user]
     @chatroom = Chatroom.chatroom_for_users(users)
-    # @messages = @chatroom.messages.order(created_at: :desc).limit(100).reverse
-    binding.pry
-    # @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
     redirect_to chatroom_path(@chatroom)
   end
 
